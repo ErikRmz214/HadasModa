11804e8804811e1ffb62dd31e6208c027770b612
ErikRmz <erik.alj_ram@hotmail.com>
2024-12-16 12:08:13 -0600

    finalizacion index

--- js/contacto.js
+++ js/contacto.js
@@ -2,7 +2,11 @@ function redirectToEmail() {
     window.location.href = 'mailto:hadasmoda@gmail.com';
 }
 
-function redirectToWhatsApp() {
+function redirectToWhatsApp1() {
+    window.location.href = 'https://wa.me/4491062225';
+}
+
+function redirectToWhatsApp2() {
     window.location.href = 'https://wa.me/4951019058';
 }
 
