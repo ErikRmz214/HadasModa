c99e0cdc31a94983dc65f3690ddd7a4624517362
ErikRmz <erik.alj_ram@hotmail.com>
2024-12-16 11:49:28 -0600

    segundo commit

---
+++ js/contacto.js
@@ -0,0 +1,11 @@
+function redirectToEmail() {
+    window.location.href = 'mailto:hadasmoda@gmail.com';
+}
+
+function redirectToWhatsApp() {
+    window.location.href = 'https://wa.me/4951019058';
+}
+
+function redirectToFacebook() {
+    window.location.href = 'https://web.facebook.com/profile.php?id=100065108679037';
+}
